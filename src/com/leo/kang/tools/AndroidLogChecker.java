a93e0e99eca480ad3cc10c886d403e7831ca0cad
unknown <yi.kang@kangyi.autonavi.com>
2014-07-31 20:09:36 +0800

    1. Add gitignore. 2. Add some comments.

--- src/com/leo/kang/tools/AndroidLogChecker.java
+++ src/com/leo/kang/tools/AndroidLogChecker.java
@@ -1,10 +1,8 @@
 package com.leo.kang.tools;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +15,8 @@ public class AndroidLogChecker {
 	// 递归显示D:test\\src下所有文件夹及其中文件
 	File root = new File("D:test\\src");
 	try {
-	    showAllFiles(root);
+	    getAllFiles(root);
 	    for (File file : fileList) {
-		// System.out.println(file.getAbsolutePath());
 		readFileByLine(file);
 	    }
 	} catch (Exception e) {
@@ -28,7 +25,14 @@ public class AndroidLogChecker {
 	}
     }
 
-    final static void showAllFiles(File dir) throws Exception {
+    /**
+     * Get all files
+     * @author: yi.kang
+     * @date: 2014年7月31日 下午8:05:25
+     * @param dir
+     * @throws Exception
+     */
+    final static void getAllFiles(File dir) throws Exception {
 	File[] fs = dir.listFiles();
 	if (fileList == null) {
 	    fileList = new ArrayList<File>();
@@ -36,40 +40,48 @@ public class AndroidLogChecker {
 	String path = "";
 	for (int i = 0; i < fs.length; i++) {
 	    path = fs[i].getAbsolutePath();
-	    // System.out.println(path);
 	    if (!fs[i].isDirectory()
 		    && path.substring(path.lastIndexOf(".")).contains("java")) {
 		fileList.add(fs[i]);
 	    }
-	    // System.out.println(fs[i].getAbsolutePath());
 	    if (fs[i].isDirectory()) {
 		try {
-		    showAllFiles(fs[i]);
+		    getAllFiles(fs[i]);
 		} catch (Exception e) {
+		    e.printStackTrace();
 		}
 	    }
 	}
     }
 
+    /**
+     * Read file line by line
+     * @author: yi.kang
+     * @date: 2014年7月31日 下午8:06:20
+     * @param file
+     */
     public static void readFileByLine(File file) {
 	try {
 	    // read file content from file
-	    StringBuffer sb = new StringBuffer("");
-
-	    FileReader reader = new FileReader(file.getAbsoluteFile());
+	    //StringBuffer sb = new StringBuffer("");
+	    String fileName = file.getAbsolutePath();
+	    FileReader reader = new FileReader(fileName);
 	    BufferedReader br = new BufferedReader(reader);
 
 	    String str = null;
 
 	    int line = 0;
+	    
 	    while ((str = br.readLine()) != null) {
 		line++;
-		if (str.contains("Log.i(") || str.contains("Log.d(")
-			|| str.contains("Log.w(") || str.contains("Log.v(")
+		if (str.contains("Log.i(") 
+			|| str.contains("Log.d(")
+			|| str.contains("Log.w(") 
+			|| str.contains("Log.v(")
 			|| str.contains("Log.e(")) {
-		    sb.append(file.getAbsolutePath().substring(
-			    file.getAbsolutePath().lastIndexOf("\\") + 1)
-			    + "  line " + line + "  " + str.trim() + "/n");
+		    // sb.append(fileName.substring(fileName.lastIndexOf("\\") +
+		    // 1)
+		    // + "  line " + line + "  " + str.trim() + "/n");
 
 		    System.out.println(file.getAbsolutePath().substring(
 			    file.getAbsolutePath().lastIndexOf("\\") + 1)
@@ -81,12 +93,12 @@ public class AndroidLogChecker {
 	    reader.close();
 
 	    // write string to file
-	    FileWriter writer = new FileWriter("c://test2.txt");
-	    BufferedWriter bw = new BufferedWriter(writer);
-	    bw.write(sb.toString());
+	    //FileWriter writer = new FileWriter("c://test2.txt");
+	    //BufferedWriter bw = new BufferedWriter(writer);
+	    //bw.write(sb.toString());
 
-	    bw.close();
-	    writer.close();
+	    //bw.close();
+	    //writer.close();
 	} catch (FileNotFoundException e) {
 	    e.printStackTrace();
 	} catch (IOException e) {
